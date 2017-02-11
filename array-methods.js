d0fde3c0e96df76ab4892c19ff345426e4a44ef1
Joshua Lee <joshualee@iconfigure.org>
2017-02-11 10:49:04 -1000

    updated code stuck at sum

--- array-methods.js
+++ array-methods.js
@@ -5,12 +5,27 @@ var dataset = require('./dataset.json');
   greater than 100000.00
   assign the resulting array to `hundredThousandairs`
 */
-var hundredThousandairs = null;
+var bankBalances = dataset.bankBalances;
+
+function bigBankBalance (e, i, arr) {
+  if(e.amount > 100000) {
+
+    return true;
+
+  }else{
+    return false;
+  }
+
+}
+
+var hundredThousandairs = bankBalances.filter(bigBankBalance);
+
+
 
 /*
   set a new key for each object in bankBalances named `rounded`
   the value of this key will be the `amount` rounded to the nearest dollar
-  example 
+  example
     {
       "amount": "134758.44",
       "state": "HI",
@@ -18,22 +33,49 @@ var hundredThousandairs = null;
     }
   assign the resulting array to `roundedDollar`
 */
-var roundedDollar = null;
+
+var roundedDollar = bankBalances.map((e, i, arr) => {
+  let rounded = Math.round(e.amount);
+  return {
+      amount: e.amount,
+      state: e.state,
+      rounded: parseInt(rounded)
+  };
+});
+// console.log('dollar', roundedDollar)
 
 /*
   set a the `amount` value for each object in bankBalances
   to the value of `amount` rounded to the nearest 10 cents
-  example 
+  example
     {
       "amount": 134758.4,
       "state": "HI"
     }
   assign the resulting array to `roundedDime`
 */
-var roundedDime = null;
+var roundedDime = bankBalances.map((e, i, arr) => {
+  e.amount = (Math.round(e.amount * 10) / 10).toFixed(1);
+
+  return {
+      amount: parseFloat(e.amount),
+      state: e.state
+  };
+});
+
+// console.log('round dime', roundedDime);
 
 // set sumOfBankBalances to the sum of all amounts in bankBalances
-var sumOfBankBalances = null;
+function sum(prev, curr, i, arr) {
+ // prev = arr[i];
+ // curr = arr[i].amount;
+
+ console.log('prev', curr.amount)
+ return prev + curr.amount;
+}
+var sumOfBankBalances = bankBalances.reduce(sum, 0);
+
+console.log('sum', sumOfBankBalances)
 
 /*
   set sumOfInterests to the sum of the 18.9% interest
@@ -77,14 +119,14 @@ var stateSums = null;
 
 /*
   set lowerSumStates to an array containing
-  only the two letter state abbreviation of each state 
+  only the two letter state abbreviation of each state
   where the sum of amounts in the state is
     less than 1,000,000
  */
 var lowerSumStates = null;
 
 /*
-  set higherStateSums to be the sum of 
+  set higherStateSums to be the sum of
     all amounts of every state
     where the sum of amounts in the state is
       greater than 1,000,000
@@ -107,7 +149,7 @@ var areStatesInHigherStateSum = null;
 
 /*
   Stretch Goal && Final Boss
-  
+
   set anyStatesInHigherStateSum to be true if
     any of these states have a sum of account values
       greater than 2,550,000
